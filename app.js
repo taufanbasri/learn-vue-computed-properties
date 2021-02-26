3500b699738f9541171eb0d781314f0779478b6a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-02-26 22:12:05 +0700

    Two Way Binding

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ const app = Vue.createApp({
   },
   methods: {
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
-      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
     }
   }
 });
